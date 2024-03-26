e29953e9942f500e668b5ed820c54691fe5da00d
Austin Putman <austin@rawfingertips.com>
2024-03-26 07:57:47 -0700

    2024 landing page

--- prisma/seed.js
+++ prisma/seed.js
@@ -29,7 +29,33 @@ async function main() {
             },
             testid: "awesome_2023"
         }
-    })
+    });
+    const party_2024 = await prisma.event.upsert({
+        where: {testid: "awesome_2024"},
+        update: {},
+        create: {
+            name: "Awesome 2024",
+            registrationStartDate: "2024-07-01T00:00:00Z",
+            startDate: "2023-09-25T14:00:00-07:00",
+            endDate: "2023-09-29T12:00:00-07:00",
+            maxAttendance: 150,
+            prices: {
+                secret: {
+                    cashmoney: 0,
+                    specialgueststar: 0,
+                },
+                "public": {
+                    "discount3": 30,
+                    "discount2": 80,
+                    "discount1": 120,
+                    "default": 150,
+                    "supporter1": 200,
+                }
+            },
+            testid: "awesome_2024"
+        }
+    });
+    console.log(`Created event ${party_2024.testid} with id: ${party_2024.id}`);
     console.log(`Created event ${party.testid} with id: ${party.id}`)
     console.log(`Seeding finished.`)
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/registration">Register Now</a>
+<head>
+    <style>
+        body {
+            background: fuchsia;
+            color: white;
+        }
+        .container {
+            border: 3px solid yellow;
+            margin-top: 100px;
+        }
+        .inner-container {
+            border: 3px solid yellow;
+            margin: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+        .inner {
+            text-align: center;
+            margin: 20% 10px;
+        }
+    </style>
+
+</head>
+<body>
+
+    <div class="inner-container">
+        <div class="inner-container">
+            <div class="inner-container">
+                <div class="inner-container">
+                    <div class="inner-container">
+        <div class="inner">
+            <h1>2024 will be Awesome</h1>
+            <p>September 25-29, 2024</p>
+            <p>Mendocino Woodlands</p>
+        </div>
+    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--<a href="/registration">Register Now</a>-->
+</body>
